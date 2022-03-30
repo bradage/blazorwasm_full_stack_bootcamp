f4cdfe5e7d01154a06b030fbdf3db22f06438f61
Bradey Merrill <bradey@itspinetech.com>
2022-03-30 17:19:52 -0600

    implement bananaservice and event with c#

--- BlazorBattles/BlazorBattles/Client/Program.cs
+++ BlazorBattles/BlazorBattles/Client/Program.cs
@@ -1,11 +1,8 @@
+using BlazorBattles.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazorBattles.Client
@@ -18,6 +15,7 @@ namespace BlazorBattles.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IBananaService, BananaService>();
 
             await builder.Build().RunAsync();
         }
--- BlazorBattles/BlazorBattles/Client/obj/Debug/net5.0/RazorDeclaration/Shared/TopMenu.razor.g.cs
+++ BlazorBattles/BlazorBattles/Client/obj/Debug/net5.0/RazorDeclaration/Shared/TopMenu.razor.g.cs
@@ -13,76 +13,83 @@ namespace BlazorBattles.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 1 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 2 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 3 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 4 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 5 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 6 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 7 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 8 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 9 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using BlazorBattles.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\_Imports.razor"
+#line 10 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
 using BlazorBattles.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
-    public partial class TopMenu : Microsoft.AspNetCore.Components.ComponentBase
+#nullable restore
+#line 11 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\_Imports.razor"
+using BlazorBattles.Client.Services;
+
+#line default
+#line hidden
+#nullable disable
+    public partial class TopMenu : Microsoft.AspNetCore.Components.ComponentBase, IDisposable
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
@@ -90,19 +97,31 @@ using BlazorBattles.Client.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 10 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\blazorbattles\Client\Shared\TopMenu.razor"
+#line 13 "C:\mygit\blazorwasm_full_stack_bootcamp\BlazorBattles\BlazorBattles\Client\Shared\TopMenu.razor"
        
     [Parameter]
     public int Bananas { get; set; } = 100;
+
+    protected override void OnInitialized()
+    {
+        BananaService.OnChange += StateHasChanged;
+    }
+
+    public void Dispose()
+    {
+        BananaService.OnChange -= StateHasChanged;
+    }
 
     public void AddBananas(int value)
     {
         Bananas += value;
     }
 
+
 #line default
 #line hidden
 #nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IBananaService BananaService { get; set; }
     }
 }
 #pragma warning restore 1591
