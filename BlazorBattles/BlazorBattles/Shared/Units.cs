614ec010c0890f5001855933a07ca25ab11321e9
Bradey Merrill <bradey@itspinetech.com>
2022-03-31 12:33:29 -0600

    create units and userunit classes

---
+++ BlazorBattles/BlazorBattles/Shared/Units.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorBattles.Shared
+{
+    public class Units
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int HitPoints { get; set; } = 100;
+        public int BananaCost { get; set; }
+    }
+}
