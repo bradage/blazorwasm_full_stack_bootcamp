5e227b4eb33db3e241a8fd58152dc897fbc00517
Bradey Merrill <bradey@itspinetech.com>
2022-04-05 15:07:44 -0600

    implement authrepository and create user class

--- BlazorBattles/BlazorBattles/Server/Startup.cs
+++ BlazorBattles/BlazorBattles/Server/Startup.cs
@@ -27,6 +27,7 @@ namespace BlazorBattles.Server
             services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
