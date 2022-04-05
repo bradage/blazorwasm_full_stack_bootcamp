8b76e6f68a7556b94b3cfea06f36d531471c90e5
Bradey Merrill <bradey@itspinetech.com>
2022-04-05 17:29:24 -0600

    create user controller

---
+++ BlazorBattles/BlazorBattles/Server/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using BlazorBattles.Server.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getbananas")]
+        public async Task<IActionResult> GetBananas()
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+            return Ok(user.Bananas);
+        }
+    }
+}
