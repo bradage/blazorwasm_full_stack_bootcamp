75b81edff7b8f9ffb1f1cd6ddb02b8572821e088
Bradey Merrill <bradey@itspinetech.com>
2022-04-05 15:41:05 -0600

    create rigster with hash and salt with authcontroller

---
+++ BlazorBattles/BlazorBattles/Server/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using BlazorBattles.Server.Data;
+using BlazorBattles.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegister request)
+        {
+            var response = await _authRepository.Register(
+               new User
+               {
+                   Username = request.Username,
+                   Email = request.Email,
+                   Bananas = request.Bananas,
+                   DateOfBirth = request.DateOfBirth,
+                   IsConfirmed = request.IsConfirmed
+               }, request.Password);
+
+            if (!response.IsSuccessful)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
--- BlazorBattles/BlazorBattles/Server/Data/AuthRepository.cs
+++ BlazorBattles/BlazorBattles/Server/Data/AuthRepository.cs
@@ -1,23 +1,57 @@
 ﻿using BlazorBattles.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace BlazorBattles.Server.Data
 {
     public class AuthRepository : IAuthRepository
     {
-        public Task<string> Login(string email, string password)
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<string>> Login(string email, string password)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<int> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<int> { IsSuccessful = false, Message = "User already exists." };
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int> { Data = user.Id, Message = "Registration successful." };
         }
 
-        public Task<bool> UserExists(string email)
+        public async Task<bool> UserExists(string email)
         {
-            throw new System.NotImplementedException();
+            if (await _context.Users.AnyAsync(user => user.Email.ToLower().Equals(email.ToLower())))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
